160245fb1a3ad5eab083650c24dfaa831e6e6ed6
charliel29 <charliel29@nycstudents.net>
2025-12-11 09:26:35 -0500

    api

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import requests
+import tkinter
 
 
-def Disney():
+def amiibo():
     response = requests.get(f"https://www.amiiboapi.com/api/")
     if response.status_code!=200:
         print("Error fetching data!")
@@ -9,7 +10,23 @@ def Disney():
     data=response.json()
     return{
 
-        "amiiboSeries" : data["amiiboSeries"]
-        ""
-
+        "amiiboSeries" : data["amiiboSeries"],
+        "character": data["character"],
+		"gameSeries": data["gameSeries"],
+		"type": data["type"],
+        "image": data["image"],
     }
+
+
+window = tk.Tk()
+window.title("amiibo search") 
+window.geometry("400x250") 
+window.resizable(False, False)
+prompt = tk.Label(window, text="Type what you want to find on ambiibo below",
+font=("Arial", 14))
+prompt.pack(pady=10)
+entry = tk.Entry(window, font=("Arial", 14), width=30)
+entry.pack(pady=5)
+result_label = tk.Label(window, text="", font=("Arial", 14, "bold"),
+fg="blue")
+result_label.pack(pady=15)
