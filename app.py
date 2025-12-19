d5294330292611fe4fc92b70fc2129d997a4d67f
charliel29 <charliel29@nycstudents.net>
2025-12-19 09:26:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,25 +2,23 @@ import requests
 import tkinter as tk
 
 
-def amiibo():
+def Disney():
     y=search_entry.get
-    response = requests.get(f"https://www.amiiboapi.com/api/{y}")
+    response = requests.get(f"https://www.fruityvice.com/{y}")
     if response.status_code!=200:
         print("Error fetching data!")
         return None
     data=response.json()
     return{
-        "character": data["character"],
-		"gameSeries": data["gameSeries"],
-		"type": data["type"],
+        ""
     }
 
 
 window = tk.Tk()
-window.title("amiibo search") 
+window.title("Disney search") 
 window.geometry("800x500") 
 window.resizable(False, False)
-prompt = tk.Label(window, text="What you want to find on ambiibo?",
+prompt = tk.Label(window, text="What you like to find on Disney?",
 font=("Arial", 16))
 prompt.pack(pady=10)
 result_label = tk.Label(window, text="", font=("Arial", 14, "bold"),
@@ -30,7 +28,7 @@ search_entry = tk.Entry (window,font=("Arial", 14),width=30,)
 search_entry.pack(pady=10,)
 button = tk.Button(
 window,text="Search",
-width=15, height= 2,command=amiibo)
+width=15, height= 2,command=Disney)
 button.pack(pady=10)
 window.mainloop()
 
