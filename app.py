306e6d8f6a0757a6a9c6cea92eb61cca5b0b9124
charliel29 <charliel29@nycstudents.net>
2025-12-22 09:26:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,23 +2,26 @@ import requests
 import tkinter as tk
 
 
-def Disney():
-    y=search_entry.get
-    response = requests.get(f"https://www.fruityvice.com/{y}")
+def Fruit():
+    y=search_entry.get()
+    response = requests.get(f"https://www.fruityvice.com/api/fruit/{y}")
     if response.status_code!=200:
         print("Error fetching data!")
         return None
     data=response.json()
     return{
-        ""
-    }
-
-
+        "protein": data["nutritions"]["protein"],
+        "sugar": data["nutritions"]["sugar"],
+        "calories": data["nutritions"]['calories'],
+        "fat": data["nutritions"]["fat"]
+        }
+    
+result_label.comfig(text=data)
 window = tk.Tk()
-window.title("Disney search") 
+window.title("Fruit search") 
 window.geometry("800x500") 
 window.resizable(False, False)
-prompt = tk.Label(window, text="What you like to find on Disney?",
+prompt = tk.Label(window, text="What fruit would you like to search up?",
 font=("Arial", 16))
 prompt.pack(pady=10)
 result_label = tk.Label(window, text="", font=("Arial", 14, "bold"),
@@ -28,7 +31,7 @@ search_entry = tk.Entry (window,font=("Arial", 14),width=30,)
 search_entry.pack(pady=10,)
 button = tk.Button(
 window,text="Search",
-width=15, height= 2,command=Disney)
+width=15, height= 2,command=Fruit)
 button.pack(pady=10)
 window.mainloop()
 
